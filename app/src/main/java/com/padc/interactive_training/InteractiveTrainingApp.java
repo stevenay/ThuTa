f5b427941e2a6eba96183161a0e6a679601d8bf2
SteveNay <naylinaung.softwarearchitect@gmail.com>
2016-09-04 16:05:00 +0630

    Added Application Object

---
+++ app/src/main/java/com/padc/interactive_training/InteractiveTrainingApp.java
@@ -0,0 +1,22 @@
+package com.padc.interactive_training;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Created by Dell on 9/4/2016.
+ */
+public class InteractiveTrainingApp extends Application {
+
+    private static Context context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = getApplicationContext();
+    }
+
+    public static Context getContext() {
+        return context;
+    }
+}

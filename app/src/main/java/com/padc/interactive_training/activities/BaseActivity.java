b89fa9070d175f91607461f4d1b5982d253d8bf1
SteveNay <naylinaung.softwarearchitect@gmail.com>
2016-09-05 15:56:41 +0630

    Added BaseActivity and Required Gradle Dependencies

---
+++ app/src/main/java/com/padc/interactive_training/activities/BaseActivity.java
@@ -0,0 +1,70 @@
+package com.padc.interactive_training.activities;
+
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.padc.interactive_training.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by Dell on 9/1/2016.
+ */
+public class BaseActivity extends AppCompatActivity {
+
+    @Nullable
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @BindView(R.id.tv_screen_title)
+    TextView tvScreenTitle;
+
+    private MenuItem inboxMenuItem;
+
+    @Override
+    public void setContentView(int layoutResID) {
+        super.setContentView(layoutResID);
+        bindViews();
+    }
+
+    protected void bindViews() {
+        ButterKnife.bind(this);
+        setupToolbar();
+    }
+
+    public void setContentViewWithoutInject(int layoutResId) {
+        super.setContentView(layoutResId);
+    }
+
+    protected void setupToolbar() {
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        inboxMenuItem = menu.findItem(R.id.action_profile);
+        inboxMenuItem.setActionView(R.layout.menu_item_view);
+        return true;
+    }
+
+    public Toolbar getToolbar() {
+        return toolbar;
+    }
+
+    public MenuItem getInboxMenuItem() {
+        return inboxMenuItem;
+    }
+
+    public TextView getScreenTitle() {
+        return tvScreenTitle;
+    }
+}

7a4d22fb7c9b61800d823c533cc4d01cb6c56c2c
SteveNay <naylinaung.softwarearchitect@gmail.com>
2016-09-05 15:51:49 +0630

    Added Basic Utils and Components classes

---
+++ app/src/main/java/com/padc/interactive_training/components/MMTextView.java
@@ -0,0 +1,31 @@
+package com.padc.interactive_training.components;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import com.padc.interactive_training.utils.MMFontUtils;
+
+/**
+ * Created by Dell on 8/30/2016.
+ */
+public class MMTextView extends TextView {
+
+    public MMTextView(Context context) {
+        super(context);
+        if (!isInEditMode())
+            MMFontUtils.setMMFont(this);
+    }
+
+    public MMTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        if (!isInEditMode())
+            MMFontUtils.setMMFont(this);
+    }
+
+    public MMTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        if (!isInEditMode())
+            MMFontUtils.setMMFont(this);
+    }
+}

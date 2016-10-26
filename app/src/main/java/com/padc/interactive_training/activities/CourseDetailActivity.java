4d09f78140baa876465cb7d1ae18ffac337e1c54
SteveNay <naylinaung.softwarearchitect@gmail.com>
2016-10-26 18:23:57 +0630

    Header Complete

--- app/src/main/java/com/padc/interactive_training/activities/CourseDetailActivity.java
+++ app/src/main/java/com/padc/interactive_training/activities/CourseDetailActivity.java
@@ -1,13 +1,27 @@
 package com.padc.interactive_training.activities;
 
 import android.content.Intent;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.padc.interactive_training.InteractiveTrainingApp;
 import com.padc.interactive_training.R;
+import com.padc.interactive_training.components.PageIndicatorView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class CourseDetailActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @BindView(R.id.collapsing_toolbar)
+    CollapsingToolbarLayout collapsingToolbar;
 
     private static final String IE_COURSE_TITLE = "IE_COURSE_TITLE";
 
@@ -21,5 +35,20 @@ public class CourseDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_detail);
+        ButterKnife.bind(this, this);
+
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowTitleEnabled(true);
+        }
+        collapsingToolbar.setTitle(" ");
+    }
+
+    private void bindData()
+    {
+
     }
 }
